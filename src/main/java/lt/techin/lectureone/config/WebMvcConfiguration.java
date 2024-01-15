fac05467d0f3b35226dc79a2eb7e78e69ad14ca5
Rūta Rožėnė <rozene.ruta@gmail.com>
2024-01-15 11:15:59 +0200

    WebMvcConfiguration class updated with ApplicationConversionService for enum mepping to case insensitive

--- src/main/java/lt/techin/lectureone/config/WebMvcConfiguration.java
+++ src/main/java/lt/techin/lectureone/config/WebMvcConfiguration.java
@@ -1,12 +1,20 @@
 package lt.techin.lectureone.config;
 
+import org.springframework.boot.convert.ApplicationConversionService;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
         configurer.defaultContentType(MediaType.APPLICATION_JSON);
+    }
+
+    public void addFormatters(FormatterRegistry registry) {
+        ApplicationConversionService.configure(registry);
     }
 }
